43e406d36c65342b40bd2427ef9a9d9f1b6f1e28
Ravi Haksar <rhakczar@gmail.com>
2019-01-22 10:25:55 -0800

    Added docstrings and comments.

--- epidemics/RegionElements.py
+++ epidemics/RegionElements.py
@@ -4,10 +4,14 @@ from Element import Element
 
 
 class Region(Element):
+    """
+    Implementation of a simple region for simulating a disease epidemic.
+    """
     def __init__(self, eta, name=None, position=None, numeric_id=None,
                  model='exponential'):
         Element.__init__(self)
 
+        # states and state space definition
         self.healthy = 0
         self.infected = 1
         self.immune = 2
@@ -22,43 +26,77 @@ class Region(Element):
         self.name = name
         self.position = position
 
+        # region model and disease spread parameter
         self.model = model
         self.eta = eta
 
+        # data structures for information from neighbors
         self.neighbors = []
         self.neighbors_states = []
         self.neighbors_types = [Region]
 
     def reset(self):
+        """
+        Reset the Region to initialization.
+        """
         self.state = self.healthy
         self.next_state = self.state
         return
 
     def update(self):
+        """
+        Set the state to the sampled next state.
+        'next' should be called prior to 'update'
+        :return:
+        """
         self.state = self.next_state
         return
 
-    def next(self, group, control):
+    def next(self, group, control=(0, 0)):
+        """
+        Sample and set the next state. Simplifies implementation of a Markov process.
+        """
+        # assume state does not change
         self.next_state = self.state
+
         if self.state != self.immune:
+            # only healthy state needs information from neighbors
             number_neighbors_on_fire = None
             if self.state == self.healthy:
                 self.query_neighbors(group)
                 number_neighbors_on_fire = self.neighbors_states.count(True)
 
+            # calculate transition probability and sample
             transition_p = self.dynamics((self.state, number_neighbors_on_fire, self.state+1), control)
             if np.random.rand() < transition_p:
                 self.next_state = self.state + 1
 
     def query_neighbors(self, group):
+        """
+        Determine whether or not neighbors are infected.
+        Supported neighbor types are defined by self.neighbors_types
+        """
         self.neighbors_states = [group[j].is_infected(group[j].state) for j in self.neighbors
                                  if any(isinstance(group[j], t) for t in self.neighbors_types)]
 
-    def dynamics(self, state_and_next_state, control):
+    def dynamics(self, state_and_next_state, control=(0, 0)):
+        """
+        Calculate a transition probability:
+            state - healthy/infected/immune, number of infected neighbors
+            next_state - healthy/infected/immune
+            control - (delta_eta, delta_nu)
+        """
         if self.model == 'exponential':
             return self.dynamics_exponential(state_and_next_state, control)
 
-    def dynamics_exponential(self, state_and_next_state, control):
+    def dynamics_exponential(self, state_and_next_state, control=(0, 0)):
+        """
+        Implementation of transition distribution. The transition from healthy to infected
+        is an exponential function of the number of infected neighbors.
+
+        Note that the process is modeled as non-self-terminating! Infected Regions will never transition
+        to immune unless control effort (delta_nu > 0) is applied!
+        """
         state, number_infected_neighbors, next_state = state_and_next_state
         delta_eta, delta_nu = control
 
@@ -84,6 +122,7 @@ class Region(Element):
             else:
                 return 0
 
+    # helper methods for querying/setting states
     def is_healthy(self, query):
         return query == self.healthy
 
--- epidemics/WestAfrica.py
+++ epidemics/WestAfrica.py
@@ -2,11 +2,29 @@ from collections import defaultdict
 import numpy as np
 
 from epidemics.RegionElements import Region
+from Simulator import Simulator
 
 
-class WestAfrica(object):
+class WestAfrica(Simulator):
+    """
+    A simulator for the 2014 Ebola outbreak in West Africa.
+    """
     def __init__(self, graph, initial_outbreak, rng=None,
                  eta=None, region_model='exponential'):
+        """
+        Initializes a simulation object. Each element is a Region.
+
+        :param graph: dictionary describing the connections between Regions. Keys should be a region name (string).
+                      Each key should reference a dictionary with keys 'edges' (neighbors)
+                      and 'pos' (position for visualization)
+        :param initial_outbreak: dictionary describing the Regions that are initially infected.
+                                 Each key should return a count of how long the Region has been infected.
+        :param rng: random number generator seed for deterministic sampling
+        :param eta: disease propagation parameter, as a dictionary with Region name as keys
+        :param region_model: simulation model for Region elements, currently only 'exponential'
+        """
+        Simulator.__init__(self)
+
         if region_model == 'exponential':
             self.eta = defaultdict(lambda: 0.08) if eta is None else eta
         elif region_model == 'linear':
@@ -15,14 +33,16 @@ class WestAfrica(object):
         self.dims = len(graph.keys())
         self.initial_outbreak = initial_outbreak
 
+        # create a collection of Regions based on provided graph
         self.group = dict()
-        self.counter = dict()
+        self.counter = dict()  # a count of how long each Region has been in the infected state
         for idx, name in enumerate(graph.keys()):
             self.group[name] = Region(self.eta[name], name=name, position=graph[name]['pos'],
                                       numeric_id=idx, model=region_model)
             self.group[name].neighbors = graph[name]['edges']
             self.counter[name] = 0
 
+            # set initial outbreak
             if name in self.initial_outbreak.keys():
                 self.group[name].set_infected()
                 self.counter[name] = self.initial_outbreak[name]
@@ -36,10 +56,15 @@ class WestAfrica(object):
         return
 
     def reset(self):
+        """
+        Reset simulation object to initialization.
+        """
+        # reset Regions and counter
         for name in self.group.keys():
             self.group[name].reset()
             self.counter[name] = 0
 
+            # reset to initial outbreak
             if name in self.initial_outbreak.keys():
                 self.group[name].set_infected()
                 self.counter[name] = self.initial_outbreak[name]
@@ -52,21 +77,39 @@ class WestAfrica(object):
         return
 
     def dense_state(self):
+        """
+        Create a representation of the state of each Region.
+
+        :return: a dictionary where the Region name refers to its state
+        """
         return {name: self.group[name].state for name in self.group.keys()}
 
-    def update(self, control):
+    def update(self, control=None):
+        """
+        Update the simulator one time step.
+
+        :param control: collection to map Region name to control for each Region,
+                        which is a tuple of (delta_eta, delta_nu)
+        """
         if self.end:
             print('process has terminated')
 
+        if control is None:
+            control = defaultdict(lambda: (0, 0))
+
+        # determine next state for each Region
         for name in self.group.keys():
             self.group[name].next(self.group, control[name])
 
+        # assume simulation will end this time step
         self.end = True
         for name in self.group.keys():
+            # check if there are any infected Regions
             if self.group[name].is_infected(self.group[name].next_state):
                 self.end = False
                 self.counter[name] += 1
 
+            # apply next state to all elements
             self.group[name].update()
 
         self.iter += 1
